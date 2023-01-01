f4e2be373372f389f7c7f4dfa930b2ca09b615ac
alonbarel <alonbarel221@gmail.com>
2023-01-01 22:17:19 +0200

    asdasd

--- src/action.js
+++ src/action.js
@@ -37,33 +37,14 @@ const connect = async() => {
     }
 }
 
-const deployStack = async() => {
-    try {
-        console.log("deploying stack...");
-        console.log("Env variables", stackConfig?.Env);
-        const createStack = await api.post("/stacks?method=repository&type=2&endpointId=2",{
-            Name:PROJECT_NAME,
-            RepositoryURL: stackConfig?.GitConfig?.URL || REPO_URL,
-            ComposeFile: stackConfig?.GitConfig?.ConfigFilePath || COMPOSE_FILE,
-            Env: stackConfig?.Env || ENV,
-            repositoryAuthentication: true,
-            repositoryPassword: "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",
-            repositoryReferenceName: stackConfig?.GitConfig?.ReferenceName || BRANCH_NAME_REF,
-            repositoryUsername: "VerioN1"
-        });
-        console.log("stack deployed!", JSON.stringify(createStack.data, null, 2));
-    } catch (error) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    }
-}
-
 const deleteStack = async() => {
     try {
         console.log("initiating delete");
         const {data: getStacks} = await api.get("/stacks?filters=%7B%22EndpointID%22:2,%22IncludeOrphanedStacks%22:true%7D");
-        stackConfig = getStacks.find(stack => stack.Name === PROJECT_NAME);
+        const currentStackConfig =  getStacks.find(stack => stack.Name === PROJECT_NAME);
+        if(currentStackConfig){
+            stackConfig = currentStackConfig;
+        }
         const stackIdToDelete = getStacks.find(stack => stack.Name === PROJECT_NAME)?.Id;
         if(stackIdToDelete){
             console.log("deleting stack", stackIdToDelete);
@@ -81,6 +62,33 @@ const deleteStack = async() => {
         });
         await Promise.allSettled(removeImages);
 
+    } catch (error) {
+      console.log(error.response.data);
+      if(error.response.data.message.includes("Stopping")){
+        console.log("stack is stopping, waiting 10 seconds");
+        await sleep(10000);
+        await deleteStack();
+      }
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    }
+}
+
+const deployStack = async() => {
+    try {
+        console.log("deploying stack...");
+        console.log("Env variables", stackConfig?.Env);
+        const createStack = await api.post("/stacks?method=repository&type=2&endpointId=2",{
+            Name:PROJECT_NAME,
+            RepositoryURL: stackConfig?.GitConfig?.URL || REPO_URL,
+            ComposeFile: stackConfig?.GitConfig?.ConfigFilePath || COMPOSE_FILE,
+            Env: stackConfig?.Env || ENV,
+            repositoryAuthentication: true,
+            repositoryPassword: "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",
+            repositoryReferenceName: stackConfig?.GitConfig?.ReferenceName || BRANCH_NAME_REF,
+            repositoryUsername: "VerioN1"
+        });
+        console.log("stack deployed!", JSON.stringify(createStack.data, null, 2));
     } catch (error) {
       console.log(error.response.data);
       console.log(error.response.status);
